be90e6f41b39950e2610c4aff40c64563c394417
sagar rathi <blastofftopluto@gmail.com>
2023-03-26 01:56:07 +0530

    unalbe to work on eq operator

--- 10_exit_status.sh
+++ 10_exit_status.sh
@@ -4,3 +4,15 @@
 # 0 success
 # 1-255 error
 
+HOST="google.com"
+
+ping -c 1 $HOST
+
+# echo "exit code is ======>$?"
+
+if [ "$?" -eq "0"]
+then 
+    echo "$HOST reachable"
+else 
+    echo "$HOST unreachable"
+fi
---
+++ notes.sh
@@ -0,0 +1,7 @@
+# 1. Command for date: 
+(date +%F)
+
+# 2. contians retur code for previously code
+ echo "$?"
+
+ # 3. 
